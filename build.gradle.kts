d7663fcb23ad75c2268959b9bae8724d086d0c1d
XChen446 <3088576338@qq.com>
2025-10-04 23:23:56 +0800

    切换插件描述生成为DSL，并更新版本号

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
+import xyz.jpenilla.resourcefactory.bukkit.Permission
+
 plugins {
     kotlin("jvm") version "2.2.0"
     id("com.gradleup.shadow") version "8.3.0"
-    id("xyz.jpenilla.run-paper") version "2.3.1"
+    id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.1"
+    id("xyz.jpenilla.run-paper") version "3.0.1"
 }
 
+
 group = "suki.mrhua269"
-version = "0.72.1"
+version = "0.72.2"
+description = "ToNeko plugin reforged version"
 
 repositories {
     mavenCentral()
@@ -26,6 +31,23 @@ tasks {
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
         minecraftVersion("1.21.8")
+    }
+}
+
+paperPluginYaml {
+    // Defaults for name, version, and description are inherited from the Gradle project
+    main = "suki.mrhua269.tnbr.ToNekoBukkitReforged"
+    authors.addAll("MrHua269","CrystalNeko")
+    // configure fields...
+    apiVersion = "1.21.8"
+    foliaSupported = true
+    permissions {
+        register("toneko.command.give") {
+            default = Permission.Default.OP
+        }
+        register("toneko.command.set") {
+            default = Permission.Default.OP
+        }
     }
 }
 
